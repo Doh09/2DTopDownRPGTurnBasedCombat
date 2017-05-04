cb8dd4d689d4e913ae3609d480bbe59d4f6dbd2c
blackjuiceXD <blackjuicexd@hotmail.com>
2017-05-04 10:47:00 +0200

    added NPCCollide and GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    private Camera camera;
+    private bool sceneChangeInitialized = false;
+    private SimpleBlit simpleBlit;
+    private CharacterScript characterScript;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            if (instance != this)
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    camera = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+	    if (sceneChangeInitialized && simpleBlit.currentValue >= 1f)
+	    {
+            SceneManager.LoadScene("BattleScene");
+	        sceneChangeInitialized = false;
+	    }
+    }
 
-
+    public void ChangeToBattleScene(List<CharacterScript> characterScripts)
+    {
+        simpleBlit = camera.GetComponent<SimpleBlit>();
+        simpleBlit.Activated = true;
+        Debug.Log("LoadMe");
+        sceneChangeInitialized = true;
+    }
     
 }
---
+++ Assets/Scripts/NPCCollide.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NPCCollide : MonoBehaviour
+{
+
+    private static GameManager gameManager;
+    private CharacterScript[] _characterScripts;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        if (gameManager == null)
+      gameManager = GameManager.instance;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("trigger");
+        if (!col.CompareTag("Enemy"))
+        {
+            return;
+        }
+        else
+        {
+            _characterScripts = col.gameObject.GetComponentsInChildren<CharacterScript>();
+
+            var player = transform.GetComponent<CharacterScript>();
+            Debug.Log("Player -- " + "Armor: " + player.armorType.ToString()  +" SkinType: " + player.skinType.ToString());
+            testmethod();
+            //gameManager.ChangeToBattleScene();
+        }
+    }
+
+    void testmethod()
+    {
+        foreach (var bitches in _characterScripts)
+        {
+            Debug.Log("Enemy Name: " + bitches.characterName);
+        }
+    }
+
+}
