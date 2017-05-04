f3158684e307c7d23c06eab88230ea969e00cb95
Doh09 <alexandreasen09@gmail.com>
2017-05-04 10:34:49 +0200

    CHAAANGES

--- Assets/BattleScene/BattleEffects/BE_Scripts/Base_Scripts/Ability.cs
+++ Assets/BattleScene/BattleEffects/BE_Scripts/Base_Scripts/Ability.cs
@@ -8,6 +8,8 @@ public abstract class Ability : ScriptableObject
     public Sprite aSprite;
     public AudioClip aSound;
     public float aBaseCoolDown = 1f;
+    public bool canTargetAllies;
+    public bool canTargetEnemies;
     
     public abstract void Initialize(GameObject obj);
     public abstract void TriggerAbility(Transform user, Transform target, BattleManager.MakeDamageText makeDmgText);// Transform abilityEffect,
