5b5dde247debb6f445591196ca56930165971204
Doh09 <alexandreasen09@gmail.com>
2017-05-10 16:25:00 +0200

    forgot to write add . before last commit

---
+++ Assets/BattleScene/BattleEffects/BE_Scripts/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float angle = 1f;
+    public bool reverseRotation = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!reverseRotation)
+		transform.Rotate(Vector3.forward, angle);
+        else
+        {
+            transform.Rotate(Vector3.back, angle);
+        }
+	}
+}
